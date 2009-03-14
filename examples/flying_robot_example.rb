2a5e3b2d2e5e2629f79cc370765e122f716f2a26
deadprogrammer <ron.evans@gmail.com>
2009-03-14 14:30:43 -0700

    Complete example for minimum flying_robot implementaion

--- examples/flying_robot_example.rb
+++ examples/flying_robot_example.rb
@@ -1,3 +1,26 @@
+# Example code for flying_robot
+# Uses Ruby Arduino Development to create a library/interface for Unmanned Aerial Vehicles
+# Written by Ron Evans (http://deadprogrammersociety.com)
+#
+# In order to implement a sketch that uses the flying_robot parser, you need to implement the methods that make up its interface
+# so that it will respond to the standard command set.
+# 
+# The following commands are supported:
+# (h)ail - See if the UAV can still respond. Should send "Roger" back.
+# (s)tatus - Grab a snapshot of all instrument readings plus any other status info that the UAV can support
+# (e)levators - Set the elevators. The command supports two parameters:
+#   direction - enter 'u' for up, 'c' for centered, or 'd' for down
+#   deflection - enter an angle between 0 and 90 degrees
+# (r)udder - Set the rudder. This command supports two parameters:
+#   direction - enter 'l' for left, 'c' for centered, or 'r' for right
+#   deflection - enter an angle between 0 and 90 degrees
+# (t)hrottle - Set the throttle. This command supports two parameters:
+#   direction - enter 'f' for forward, or 'r' for reverse
+#   speed - enter a percentage from 0 to 100
+# (i)nstruments - Read the current data for one of the installed instruments on the UAV. This command supports one parameter:
+#   id - enter an integer for which instrment readings should be returned from. If there is not an instrument installed
+#        for that id 'Invalid instrument' should be returned
+#
 class FlyingExample < ArduinoSketch
   serial_begin :rate => 9600
   
